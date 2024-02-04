9fd56faff888607a74704310570559a5a299309d
Pratyush Semwal <pratyushsemwal2004@gmail.com>
2024-02-04 15:43:01 +0530

    Matrix addition

--- sum2d.c
+++ sum2d.c
@@ -1,73 +1,3 @@
-/*
-#include <stdio.h>
-
-int main()
-{
-    int rows1, cols1, rows2, cols2;
-
-    // Prompt the user to enter the number of rows and columns in the array
-    printf("Enter the number of rows and columns in the matrix 1: ");
-    scanf("%d %d", &rows1, &cols1);
-
-    //Prompt the user to enter the number of rows and columns in the array
-    printf("Enter the number of rows and columns in the matrix 2: ");
-    scanf("%d %d", &rows2, &cols2);
-
-    if (rows1 != rows2 || cols1 != cols2)
-    {
-        printf("Matrix addition not allowed!\n");
-    }
-
-    else
-    {
-
-        // Declare a 2D array with the specified number of rows and columns 
-        int arr1[100][100] = {0}, arr2[100][100] = {0}, sum[100][100] = {0};
-
-        // Prompt the user to enter the elements of the array
-        printf("Enter the elements of the matrix 1:\n");
-        for (int i = 0; i < rows1; i++)
-        {
-            for (int j = 0; j < cols1; j++)
-            {
-                printf("\nEnter the element at position (%d, %d) : ", i + 1, j + 1);
-                scanf("%d", &arr1[i][j]);
-            }
-        }
-
-        // Prompt the user to enter the elements of the matrix 2
-        printf("Enter the elements of the array:\n");
-        for (int i = 0; i < rows2; i++)
-        {
-            for (int j = 0; j < cols2; j++)
-            {
-                printf("\nEnter the element at position (%d, %d) : ", i + 1, j + 1);
-                scanf("%d", &arr2[i][j]);
-            }
-        }
-
-        for (int i = 0; i < rows1; i++)
-        {
-            for (int j = 0; j < cols1; j++)
-            {
-                sum[i][j] = arr1[i][j] + arr2[i][j];
-            }
-        }
-        // Print the elements of the array 
-        printf("The Sum matrix is:\n");
-        for (int i = 0; i < rows1; i++)
-        {
-            for (int j = 0; j < cols1; j++)
-            {
-                printf("%d ", sum[i][j]);
-            }
-            printf("\n");
-        }
-    }
-    return 0;
-}
-*/
-
 #include <stdio.h>
 
 int main()
@@ -75,11 +5,11 @@ int main()
     int rows1, cols1, rows2, cols2;
 
     /* Prompt the user to the number of rows columns in the array */
-    printf("Enter the number rows and columns in the first array: ");
+    printf("Enter the number rows and columns in the first matrix 1: ");
     scanf("%d %d", &rows1, &cols1);
 
     /* Prompt the user to enter the number of rows and columns in the array */
-    printf("Enter the number of rows and columns in the second array: ");
+    printf("Enter the number of rows and columns in the second matrix 2: ");
     scanf("%d %d", &rows2, &cols2);
 
     /* Check if the arrays can be added */
